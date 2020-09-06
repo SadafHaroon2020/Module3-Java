b8d720d96928f7e7a44a2a424c7e553af393859a
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-09-06 12:15:45 -0500

    Deploying/Creating my first SpringBoot project using Github

---
+++ SprinMCVLab01/src/main/java/com/sadaf/SprinMCVLab01/SprinMcvLab01Application.java
@@ -0,0 +1,13 @@
+package com.sadaf.SprinMCVLab01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinMcvLab01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinMcvLab01Application.class, args);
+	}
+
+}
---
+++ SprinMCVLab01/src/test/java/com/sadaf/SprinMCVLab01/SprinMcvLab01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadaf.SprinMCVLab01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinMcvLab01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
